1b37bfebf2a87bf0663af2c234e9d18fe31b47f6
BuildTools <unconfigured@null.spigotmc.org>
2020-05-29 16:02:30 +0100

    login form done

--- application.py
+++ application.py
@@ -12,12 +12,12 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 app = Flask(__name__)
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://kmvpbvadlwwnzx:163d32c01378e2740aa1e0a73454c40d863303ba40d1c2607a9354e81dd1577c@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d285f5ll83g7ec"
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)   
 
-# Configure session to use filesystem
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -33,15 +33,24 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    message = None
+    if request.method == "POST":
+        usernameinput = str(request.form.get("username_input"))
+        passwordinput = str(request.form.get("password_input"))
+        user_data = User.query.filter(User.username==usernameinput, User.password==passwordinput)
+        print(user_data)
+        count = user_data.count()
+        print(count)
+        if count > 0:
+            print("sucess")
+            return redirect("/search", code=302)
+        else:
+            message = "Please enter valid login details."  
+    return render_template("login.html", message=message)
 
-
-    #    verification = detail.password
-    #password_input = input("Type your password here:")
-    #if password_input == verification:
-     #   print("success")
-    #else: 
-     #   print("fail")
+@app.route("/search")
+def search():
+    return render_template("search.html")
 
 @app.route("/todolist")
 def todolist():
@@ -57,11 +66,3 @@ def logout():
  # session.pop('username', None)
   #flash('You have successful logged out!')
   return redirect("index.html")
-
-@app.route("/test", methods=["POST"])
-def testing():
-  
-    usernameinput = request.form.get("username_input")
-    user_count = User.query.filter_by(username=usernameinput).count()
-
-    return render_template("test.html", user_count=user_count)

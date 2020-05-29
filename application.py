d58bda4f02a353f35a6cfcf115ecb01e9cc23fc8
BuildTools <unconfigured@null.spigotmc.org>
2020-05-29 18:46:58 +0100

    registration added

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from flask_hashing import Hashing
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, Session
 from models import *
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,8 +13,6 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 app = Flask(__name__)
-hashing = Hashing(app)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)   
@@ -22,12 +20,14 @@ db.init_app(app)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+
+
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+hashing = Hashing(app)
 
 @app.route("/")
 def index():
@@ -58,27 +58,29 @@ def search():
 def todolist():
     return render_template("todolist.html")
 
-@app.route("/signup")
-
+@app.route("/register", methods=["GET", "POST"])
 
 def register():
     message = None
-    #if request.method == "POST":
-     #   usernameinput = request.form.get("username_input")
-      #  passwordinput = request.form.get("password_input")
-       # hashed_password = hashing.hash_value("passwordinput", salt="dev")
-        #print (hashed_password)
-    #    user_data = User.query.filter(User.username==usernameinput, User.password==passwordinput)
-     #   print(user_data)
-       # count = user_data.count()
-      #  print(count)
-        #if count > 0:
-         # insert 
+    if request.method == "POST":
+        newusernameinput = request.form.get("new_username_input")
+        newpasswordinput = request.form.get("new_password_input")
+        hashed_password = hashing.hash_value(newpasswordinput, salt="dev")
+        user_data = User.query.filter(User.username==newusernameinput)
+        count = user_data.count()
+        if count == 0:
+            new_user = User(username=newusernameinput, password=hashed_password)
+            db.add(new_user)
+            db.commit()
+            message = "Sucessfully registered. Please log in using the login page."
+        else:
+            message = "Already registered. Please email admin if problem persists."
 
- #        new_user = User(
-  #                  username = 'newusername',
-   #                 password = 'newpassword')
-    return render_template("signup.html")
+
+
+
+    #return redirect("/login", code=302)
+    return render_template("signup.html", message=message)
 
 
 @app.route("/logout")
